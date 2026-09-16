5744612c6735b7403e64bb08786c1c6233e1fa59
gabriela0402 <gabiromano2018@gmail.com>
2026-09-16 16:27:40 -0300

    chore: prepara ambiente para a parte 3

--- database/biblioteca_db.sql
+++ database/biblioteca_db.sql
@@ -7,6 +7,10 @@
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
+CREATE DATABASE biblioteca_db
+  CHARACTER SET utf8mb4
+  COLLATE utf8mb4_unicode_ci;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
